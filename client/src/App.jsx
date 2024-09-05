03c897a2d32c49f8465736160b6617fdcf17dfb2
wafas-repo <wafa.qazi@outlook.com>
2024-09-04 21:50:57 -0400

    Fixed error for refresh token

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <a href="http://localhost:888/login"> log in to Spotify</a>
+        
+    </>
+  )
+}
+
+export default App
